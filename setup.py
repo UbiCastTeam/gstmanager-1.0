7e3bb0ff461ffb4842a303c6b06879ece03e4067
Anthony Violo <anthony.violo@ubicast.eu>
2016-01-12 11:19:17 +0100

    Add dependencies for gstmanager installation

--- setup.py
+++ setup.py
@@ -24,7 +24,18 @@ setup(
         'gstmanager1/profiles'
     ],
     install_requires=[
+        'gir1.2-gstreamer-1.0',
+        'gstreamer1.0-tools',
+        'libgstreamer1.0-0',
+        'gstreamer1.0-plugins-base',
+        'gir1.2-gst-plugins-base-1.0',
+        'gstreamer1.0-plugins-good',
+        'libgstreamer-plugins-bad1.0-0',
+        'gstreamer1.0-plugins-bad',
+        'gstreamer1.0-plugins-ugly',
         'gi',
+        'python3-gi'
+        'python3-gst-1.0',
         'libgstreamer1.0',
     ]
 )
